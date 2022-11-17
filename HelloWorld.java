26a06d0ba6314625bf0ecdee535e5e3ebc0c9a83
sunnyCodeENGINEER <116773072+sunnyCodeENGINEER@users.noreply.github.com>
2022-11-17 00:41:04 +0000

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // implicit type conversion
+        int number = 5;
+        double number2 = number;
+    
+        System.out.println(number2);
+    }
+}
---
+++ MyFirstProject/src/App.java
@@ -0,0 +1,35 @@
+public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    static int age2 = 14;
+
+    public static void main(String[] args) throws Exception {
+       int age = 10;
+
+       // premitive data types in java.
+       // integer types
+       byte aSingleByte = 100;
+       short aShortNumber = 2000;
+       int anInteger = 2145214878;
+       long aLargeNumber = 9998977892872564L;
+
+       // decimal types
+       double aDouble = 1.787285;
+       float aFloat = 3.488926F;
+
+       // booleans
+       boolean isWeekday = true;
+       boolean isWeekend = false;
+
+       // characters
+       char copyrightSymbol = '\u00A9';
+
+       System.out.println("I am " + age + " years old. He is " + age2 + " years old");
+       System.out.println("This is the copyright symbol: " + copyrightSymbol);
+
+        
+    }
+}
+
